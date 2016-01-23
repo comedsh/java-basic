66e5b80728d57ca02617f40e7b803e40b3feeed0
shangyang <comedshang@163.com>
2016-01-23 10:25:16 +0800

    NIO Socket 和 普通 Socket 之间的兼容性

--- src/main/java/socket/nio/scenario_3/Client.java
+++ src/main/java/socket/nio/scenario_3/Client.java
@@ -1,11 +1,11 @@
 package socket.nio.scenario_3;
 
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.InetAddress;
-import java.net.Socket;
-import java.nio.ByteBuffer;  
+import java.net.Socket;  
   
 public class Client {  
 	
@@ -13,20 +13,25 @@ public class Client {
         
     	Socket server = new Socket(InetAddress.getLocalHost(), 5678);  
     	
-        BufferedReader in = new BufferedReader( new InputStreamReader( server.getInputStream() ) );  
+    	InputStream sin = server.getInputStream(); 
+    	
+        //BufferedReader bin = new BufferedReader( new InputStreamReader( sin ) );  
         
         PrintWriter out = new PrintWriter(server.getOutputStream());
         
-        while (true) {  
-        	
-            out.println( "hello, I'm Native Client ~ " );
-            
-            out.flush();
-            
-            System.out.println("read from server: " + in.readLine() );
-            
-        }
+        while(true){
         
+	        out.println( "hello, I'm Native Client ~ " );
+	        
+	        out.flush();
+	        
+	        System.out.println("read from server, length = " + sin.available()  );
+	        
+	        //System.out.println("read from server: " + bin.readLine() );
+	        
+	        Thread.sleep(1000);
+	        
+        }
     }  
     
 }  
